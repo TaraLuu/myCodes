6b06a2b9369948f01414d4a952dac677329a8d34
TaraLuu <taraluu26@gmail.com>
2022-10-28 19:49:30 -0400

    first commit

---
+++ HackerRank/26-ArithmeticOperators.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+def myFunction (a, b) :
+    sumf = a + b
+    diff = a - b
+    prof = a * b
+    print(sumf)
+    print(diff)
+    print(prof)
+
+myFunction(a, b)
